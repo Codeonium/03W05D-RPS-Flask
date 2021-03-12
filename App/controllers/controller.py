3a3d9b148c0f4a181768c669d70bb56b9bfa3529
codeonium <codeon2020@gmail.com>
2021-03-12 13:14:20 +0000

    started to work on front page

--- App/controllers/controller.py
+++ App/controllers/controller.py
@@ -4,3 +4,13 @@ from App import app
 @app.route('/')
 def index():
     return render_template('base.html', title='RPS - The Game')
+
+@app.route('/game')
+def create_game():
+    return render_template('game.html', title='RPS - The Game')
+
+@app.route('/scores')
+def return_scores():
+    return render_template('scores.html', title='RPS - The Game')
+
+
